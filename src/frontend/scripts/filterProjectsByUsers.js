58a5715a37d9bf1104b0765613d02124829734cf
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-09-01 15:18:47 -0400

    Merge files

--- src/frontend/scripts/filterProjectsByUsers.js
+++ src/frontend/scripts/filterProjectsByUsers.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-fetch('../../backend/projects.json')
-	.then((response) => {
-		if (response.ok) {
-			return response.json();
-		} else {
-			console.error(`Error cuando se fetcheo`);
-		}
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((e) => {
-		console.log(e);
-	});
-=======
 import data from '../../backend/projects.json';
 
 console.log(data);
